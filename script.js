721d652bd9830435ce33540ee95ddf2371b42fec
Ayla Dillis <aayladillis@gmail.com>
2020-06-29 16:04:58 -0700

    work with tutor

--- script.js
+++ script.js
@@ -1,42 +1,86 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+    var citybutton = document.getElementById("#city-button");
+
+    var cities = [
+        "Orlando"
+    ];
+
+    function retriveArray(){
+
+    var cities = localStorage.getItem("cities");
 
 
-//   // once search button is clicked do the following  
-//     $("#search").on("click", function(){
+    }
+
+// function to make button.links and add to page
+// arrayToUse looks at animal array
+    function populateButtons() {
+        $(citybutton).empty();
+    
+        for (var i = 0; i < cities.length; i++) {
+          var newButton = $("<button>");
+        //   newButton.addClass(classToAdd);
+          newButton.attr("data-type", cities[i]);
+          newButton.text(cities[i]);
+          $(citybutton).append(newButton);
+        }
+    
+      }
+
+  // once search button is clicked do the following  
+   $("#search").on("click", function(event){
+    event.preventDefault();
+
+    $("#city-info").empty();
+    $(".city-button").removeClass("active");
+    $(this).addClass("active");
+
+    console.log("button clicked")
+    populateButtons();
+
+    var type = $(this).attr("data-type");
+//city name is extracted from array
+    var cityName = "Orlando"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&units=imperial&appid=6d62c1e57554dc6cee60932bdfd78a07"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+   
+        .then(function(response){
+            console.log("response", response)
+
+        });
+
+   })
       
-//         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=6d62c1e57554dc6cee60932bdfd78a07";
 
-
-
-//     });
-
-//     $("#add-city").on("click", function(event) {
-//         event.preventDefault();
-//         var newCity = $("button").eq(0).val();
-
-//         if (newCity.length > 2) {
-//             city.push(newCity);
-//         }
-
-//         populateButtons(city, "city-button", "#city-button");
-//     });
-
-// });
-
-var button = document.querySelector(".button")
-var inputValue = document.querySelector(".inputValue")
-
-var name = document.querySelector(".name");
-var humidity = document.querySelector(".humidity");
-var windSpeed = document.querySelector(".wind-speed");
-var uvIndex = document.querySelector(".uv-index");
-
-button.addEventListener("click", function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=6d62c1e57554dc6cee60932bdfd78a07")
-    .then(response => response.json())
-    .then(data => console.log(data))
 
 });
+
+
+
+// var button = document.querySelector(".button")
+// var inputValue = document.querySelector(".inputValue")
+
+// var name = document.querySelector(".name");
+// var humidity = document.querySelector(".humidity");
+// var windSpeed = document.querySelector(".wind-speed");
+// var uvIndex = document.querySelector(".uv-index");
+
+// $(".button").on("click", function(event) {
+//     event.preventDefault();
+//     var newCity = $("button").eq(0).val();
+
+//     if (newCity.length > 2) {
+//         city.push(newCity);
+//     }
+
+//     populateButtons(city, "button", ".button");
+// });
+
 
 
 
